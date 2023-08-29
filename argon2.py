42773c47eb9a715279cb0194373a6a3d36a34f77
2kpr <96332338+2kpr@users.noreply.github.com>
2023-08-29 21:41:30 +0000

    Create argon2.py

---
+++ argon2.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import argon2
+import getpass
+
+key = argon2.low_level.hash_secret_raw(
+  getpass.getpass().encode(),
+  getpass.getpass().encode(),
+  time_cost=1,
+  memory_cost=1024,
+  parallelism=1,
+  hash_len=512,
+  type=argon2.low_level.Type.ID
+)
+
+with open("key", "wb") as f:
+	f.write(key)
